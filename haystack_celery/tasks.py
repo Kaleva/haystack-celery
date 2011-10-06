cb55b0a73f0662039219102011f85c47ee0a439a
Sid Mitra <sidmitra.del@gmail.com>
2011-10-07 03:42:07 +0530

    Disabled the periodic task for updating index.

--- haystack_celery/tasks.py
+++ haystack_celery/tasks.py
@@ -23,13 +23,16 @@ class SearchIndexUpdateTask(Task):
             logging.exception("Index not updated for expert")
             self.retry([app_name, model_name, pk], kwargs, exc=exc)
 
+"""
+# A sample celerybeat task for updating index
+
 class SearchIndexUpdatePeriodicTask(PeriodicTask):
     routing_key = 'periodic.search.update_index'
-    run_every = crontab(hour=4, minute=0)
+    run_every = crontab(hour=0, minute=0)
 
     def run(self, **kwargs):
         logging.info("Starting update index")
         # Run the update_index management command
         update_index.Command().handle()
         logging.info("Finishing update index")
-
+"""
