385c800e2fe17b372e8bc4f0a5c7dc9a411f6320
Sid Mitra <sidmitra.del@gmail.com>
2011-10-06 18:59:18 +0530

    Added default django logging model to indexing celery task

--- haystack_celery/tasks.py
+++ haystack_celery/tasks.py
@@ -1,10 +1,11 @@
-from django.db.models.loading import get_model
-
-from haystack import site
-from haystack.management.commands import update_index
+import logging
 
 from celery.task import Task, PeriodicTask
 from celery.schedules import crontab
+from django.db.models.loading import get_model
+from haystack import site
+from haystack.management.commands import update_index
+
 
 class SearchIndexUpdateTask(Task):
     name = "search.index.update"
@@ -13,14 +14,13 @@ class SearchIndexUpdateTask(Task):
     max_retries = 1
 
     def run(self, app_name, model_name, pk, **kwargs):
-        logger = self.get_logger(**kwargs)
         try:
             model_class = get_model(app_name, model_name)
             instance = model_class.objects.get(pk=pk)
             search_index = site.get_index(model_class)
             search_index.update_object(instance)
         except Exception, exc:
-            logger.error(exc)
+            logging.exception("Index not updated for expert")
             self.retry([app_name, model_name, pk], kwargs, exc=exc)
 
 class SearchIndexUpdatePeriodicTask(PeriodicTask):
@@ -28,9 +28,8 @@ class SearchIndexUpdatePeriodicTask(PeriodicTask):
     run_every = crontab(hour=4, minute=0)
 
     def run(self, **kwargs):
-        logger = self.get_logger(**kwargs)
-        logger.info("Starting update index")
+        logging.info("Starting update index")
         # Run the update_index management command
         update_index.Command().handle()
-        logger.info("Finishing update index")
+        logging.info("Finishing update index")
 
