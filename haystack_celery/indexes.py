139f4e0f33a4f86c69d899340882486d8aee9342
Sid Mitra <sidmitra.del@gmail.com>
2011-11-11 02:22:34 +0530

    Updated for haystack 2.x. For use with haystack 1.1.1, use tag "1.1.1"

--- haystack_celery/indexes.py
+++ haystack_celery/indexes.py
@@ -1,15 +1,14 @@
 from django.db.models import signals
 from django.db.models.loading import get_model
-
 from haystack import indexes
-from haystack import site
 
 from tasks import SearchIndexUpdateTask
 
 def remove_instance_from_index(instance):
-    model_class = get_model(instance._meta.app_label, instance._meta.module_name)
-    search_index = site.get_index(model_class)
-    search_index.remove_object(instance)
+    model = get_model(instance._meta.app_label, instance._meta.module_name)
+    unified_index = haystack_connections['default'].get_unified_index()
+    index = unified_index.get_index(model)
+    index.remove_object(instance)
 
 class QueuedSearchIndex(indexes.SearchIndex):
     """
@@ -19,17 +18,17 @@ class QueuedSearchIndex(indexes.SearchIndex):
     to update this to handle deletes as well (with a delete task).
     """
     # We override the built-in _setup_* methods to connect the enqueuing operation.
-    def _setup_save(self, model):
-        signals.post_save.connect(self.enqueue_save, sender=model)
+    def _setup_save(self):
+        signals.post_save.connect(self.enqueue_save, sender=self.get_model())
 
-    def _setup_delete(self, model):
-        signals.post_delete.connect(self.enqueue_delete, sender=model)
+    def _setup_delete(self):
+        signals.post_delete.connect(self.enqueue_delete, sender=self.get_model())
 
-    def _teardown_save(self, model):
-        signals.post_save.disconnect(self.enqueue_save, sender=model)
+    def _teardown_save(self):
+        signals.post_save.disconnect(self.enqueue_save, sender=self.get_model())
 
-    def _teardown_delete(self, model):
-        signals.post_delete.disconnect(self.enqueue_delete, sender=model)
+    def _teardown_delete(self):
+        signals.post_delete.disconnect(self.enqueue_delete, sender=self.get_model())
 
     def enqueue_save(self, instance, **kwargs):
         SearchIndexUpdateTask.delay(instance._meta.app_label, instance._meta.module_name, instance._get_pk_val())
--- haystack_celery/tasks.py
+++ haystack_celery/tasks.py
@@ -3,36 +3,33 @@ import logging
 from celery.schedules import crontab
 from celery.task import Task, PeriodicTask
 from django.db.models.loading import get_model
-from haystack import site
+from haystack import connections as haystack_connections
 from haystack.management.commands import update_index
-
 
 class SearchIndexUpdateTask(Task):
     name = "search.index.update"
-    #routing_key = 'search.index.update'
     default_retry_delay = 5 * 60
     max_retries = 1
 
     def run(self, app_name, model_name, pk, **kwargs):
         try:
-            model_class = get_model(app_name, model_name)
-            instance = model_class.objects.get(pk=pk)
-            search_index = site.get_index(model_class)
-            search_index.update_object(instance)
+            model = get_model(app_name, model_name)
+            instance = model.objects.get(pk=pk)
+            unified_index = haystack_connections['default'].get_unified_index()
+            index = unified_index.get_index(model)
+            index.update_object(instance)
         except Exception, exc:
             logging.exception("Index not updated for expert")
             self.retry([app_name, model_name, pk], kwargs, exc=exc)
 
-"""
-# A sample celerybeat task for updating index
 
 class SearchIndexUpdatePeriodicTask(PeriodicTask):
-    routing_key = 'periodic.search.update_index'
+    name = "search.index.periodic_update"
     run_every = crontab(hour=0, minute=0)
-
+    abstract = True
+    
     def run(self, **kwargs):
         logging.info("Starting update index")
-        # Run the update_index management command
         update_index.Command().handle()
         logging.info("Finishing update index")
-"""
+
